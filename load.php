4067db06440f1cb5ac6c72929845f6ebba0de26b
pavelkenarov1@gmail.com <pavel kenarov>
2020-01-26 10:15:48 +0200

    Auto format code

--- load.php
+++ load.php
@@ -4,21 +4,21 @@ require_once 'includes/config.php';
 $page = empty($_GET['p']) ? 1 : (int)$_GET['p'];
 $perPage = 30;
 $offset = ($page - 1) * $perPage;
-if($offset < 0)
-	$offset = 0;
+if ($offset < 0)
+    $offset = 0;
 
 $db = db::getInstance();
-$mysqli = $db->getConnection(); 
+$mysqli = $db->getConnection();
 
 $employees = [];
 $get = $mysqli->query(' SELECT `avatar`, `name`, `title`, `company`, `bio` FROM `employees` 
 WHERE `status` = "finished" ORDER BY ts LIMIT  ' . $offset . ',' . $perPage);
 while ($e = $get->fetch_assoc()) {
-	
-	if(empty($e['bio']))
-		unset($e['bio']);
-	
-	$employees[] = $e;
+
+    if (empty($e['bio']))
+        unset($e['bio']);
+
+    $employees[] = $e;
 }
 
 header('Content-Type: application/json');
