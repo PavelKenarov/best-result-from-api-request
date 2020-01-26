6b74807156c856aae54471bf4b7a722129a269df
Pavel Kenarov <49127682+PavelKenarov@users.noreply.github.com>
2020-01-26 10:07:25 +0200

    Update db.class.php

--- includes/db.class.php
+++ includes/db.class.php
@@ -15,18 +15,17 @@ class db {
 	@return Instance
 	*/
 	public static function getInstance() {
-		if(!self::$_instance) {
-			self::$_instance = new self();
-		}
-		return self::$_instance;
+	    if(!self::$_instance) {
+		self::$_instance = new self();
+	    }
+	    return self::$_instance;
 	}
 
 	private function __construct() {
-		
-		$this->_connection = new mysqli($this->_host, $this->_username, $this->_password, $this->_database);
-		if(mysqli_connect_error()) {
-			exit("Failed to conencto to MySQL: " . mysql_connect_error());
-		}
+	    $this->_connection = new mysqli($this->_host, $this->_username, $this->_password, $this->_database);
+	    if(mysqli_connect_error()) {
+	        exit("Failed to conencto to MySQL: " . mysql_connect_error());
+	    }
 	}
 
 	// Magic method clone is empty to prevent duplication of connection
@@ -34,7 +33,7 @@ class db {
 
 	// Get mysqli connection
 	public function getConnection() {
-		return $this->_connection;
+	    return $this->_connection;
 	}
 }
 ?>
